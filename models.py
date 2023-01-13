5066d59ed212a9fe790e1024a2ffa88a60418fad
dhamo <121689327+Dhamo2112@users.noreply.github.com>
2023-01-13 11:56:41 +0530

    Update models.py
    add an alternate model

--- models.py
+++ models.py
@@ -20,14 +20,3 @@ class expense_management(models.Model):
     
     def __str__(self):
         return self.expense
-
-
-class expense_management(models.Model):
-    expense=models.CharField(max_length=100)
-    date=models.DateTimeField()
-    amount=models.IntegerField()
-    description=models.TextField(max_length=1300)
-    Sign_In=models.ForeignKey(Sign_In, blank=True,null=True,on_delete=models.CASCADE)
-    
-    def __str__(self):
-        return self.expense
